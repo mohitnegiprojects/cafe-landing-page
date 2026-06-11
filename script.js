61ccc1d23613482ca00ce54275fdd7fe75d1d587
Mohit Negi <mohitnegiworks@gmail.com>
2026-06-11 18:40:15 +0530

    Added a Night theme button making webpage more suitable for night vision.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Top button arrow
 window.addEventListener('scroll',function(){
     const topBtn = document.getElementById('topBtn');
     if(window.scrollY>300){
@@ -8,4 +9,15 @@ window.addEventListener('scroll',function(){
 });
 document.getElementById('topBtn').addEventListener('click',function(){
     window.scrollTo({top: 0, behavior:'smooth'});
+});
+
+//Toogle button
+const themebtn = document.getElementById('theme-btn');
+themebtn.addEventListener('click',function(){
+    document.body.classList.toggle('night-mode');
+    if(document.body.classList.contains('night-mode')){
+        themebtn.textContent = "☀️Switch to Day Mode";
+    }else{
+        themebtn.textContent = "🌙Switch to Night Mode";
+    }
 });
